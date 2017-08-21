c02e0654143975ee46f5fe01b0857de9f1eee417
MasterUNG <phrombutr@gmail.com>
2017-08-21 15:57:08 +0700

    Finish 21aug

--- mobile/src/main/java/mind/chan/mean/watchchild/MainActivity.java
+++ mobile/src/main/java/mind/chan/mean/watchchild/MainActivity.java
@@ -2,6 +2,7 @@ package mind.chan.mean.watchchild;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         latTextView = (TextView) findViewById(R.id.txtLat);
         lngTextView = (TextView) findViewById(R.id.txtLng);
         button = (Button) findViewById(R.id.btnSent);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                latTextView.setText("Lat = 123.4567");
+                lngTextView.setText("Lng == 456.1234");
+            }
+        });
 
     }   // Main Method
 
