26fcc869d20c38f9136b481961a0630d94117da8
MasterUNG <phrombutr@gmail.com>
2017-08-21 15:51:31 +0700

    Design UI OK

--- mobile/src/main/java/mind/chan/mean/watchchild/MainActivity.java
+++ mobile/src/main/java/mind/chan/mean/watchchild/MainActivity.java
@@ -2,12 +2,26 @@ package mind.chan.mean.watchchild;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView latTextView, lngTextView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Initial View
+        latTextView = (TextView) findViewById(R.id.txtLat);
+        lngTextView = (TextView) findViewById(R.id.txtLng);
+        button = (Button) findViewById(R.id.btnSent);
+
+    }   // Main Method
+
+}   // Main Class
